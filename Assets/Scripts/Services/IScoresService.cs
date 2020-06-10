26848f17970cd19941fe02dd4b884fedbbe6de42
infuzo <infuzo1@gmail.com>
2020-06-10 13:49:02 +0300

    Add scores service and scores storage service.

---
+++ Assets/Scripts/Services/IScoresService.cs
@@ -0,0 +1,12 @@
+﻿namespace Stack.Services
+{
+    public interface IScoresService
+    {
+        int GetCurrentScore();
+        void IncreaseCurrentScore(int increment);
+        void ResetCurrentScore();
+        void SaveCurrentScoreIfHigher();
+        int? GetHighestScore();
+    }
+}
+
---
+++ Assets/Scripts/Services/ScoresStorageService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack.Services
+{
+    class ScoresStorageService : IScoresStorageService
+    {
+        public virtual int? GetHighestScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void SetHighestScore(int score)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
